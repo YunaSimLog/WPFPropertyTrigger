76e3906c4b1ba0faa2bdf81975353af3e74f2cf1
yunasim <ynsim@solidit.co.kr>
2024-01-26 23:14:43 +0900

    #✨feature: 소스 초기 업로드

---
+++ WpfPropertyTrigger/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation.Peers;
+using WpfBase;
+using WpfControlTemplate.Models;
+
+namespace WpfControlTemplate.ViewModels
+{
+    public class MainViewModel: ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Items = ComboItem.Items;
+        }
+
+        public List<ComboItem> Items { get; set; }
+    }
+}
